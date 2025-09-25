185662f60c1e33d2e575f56121e7d8ddd1d40ca0
Saimor Raduan <saimorraduan@gmail.com>
2025-09-25 08:16:50 -0400

    feat: add create_table.py to create table 'dados_ppm'

---
+++ src/create_table.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+import psycopg2
+from dotenv import load_dotenv
+
+def main(): 
+    # --- Carrega as variáveis de ambiente do arquivo .env ---
+    load_dotenv()
+
+    # --- Configurações do Banco de Dados usando variáveis de ambiente ---
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    database = os.getenv("DB_NAME")
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+
+    try:
+        print("Tentando estabelecer a conexão com o banco de dados...")
+        conexao = psycopg2.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=user,
+            password=password
+        )
+        print("Conexão estabelecida com sucesso!")
+        
+        cursor = conexao.cursor()
+
+        # --- Criação da Tabela (com verificação para evitar erros) ---
+        print("Verificando e criando a tabela 'dados_ppm' se ela não existir...")
+        
+        # Adiciona a verificação "IF NOT EXISTS" para evitar erros se a tabela já existir
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS dados_ppm (
+            id SERIAL PRIMARY KEY,
+            nivel_territorial_codigo INTEGER,
+            nivel_territorial VARCHAR(255),
+            unidade_de_medida_codigo INTEGER,
+            unidade_de_medida VARCHAR(255),
+            valor INTEGER,
+            municipio_codigo INTEGER, 
+            municipio VARCHAR(255),
+            ano_codigo INTEGER,
+            ano INTEGER,
+            variavel_codigo INTEGER,
+            variavel VARCHAR(255),
+            tipo_rebanho_codigo INTEGER,
+            tipo_rebanho VARCHAR(255)
+        );
+        """
+        cursor.execute(create_table_query)
+        
+        conexao.commit()
+        print("Tabela 'dados_ppm' verificada/criada com sucesso.")
+
+    except Exception as e:
+        print(f"Erro: {e}")
+
+    finally:
+        if 'conexao' in locals() and conexao:
+            cursor.close()
+            conexao.close()
+            print("Conexão com o banco de dados fechada.")
+
+if __name__ == "__main__":
+    main()
